73ce2bc9134dfe1b94f3c5467b3674f2cb079e4a
ZiluTian <zilu.tian@epfl.ch>
2020-09-22 18:46:01 +0200

    Group submission

--- lab1/SubList.scala
+++ lab1/SubList.scala
@@ -10,21 +10,36 @@ object SubList {
     case (Cons(x, xs), Cons(y, ys)) => (x == y && subList(xs, ys)) || subList(l1, ys)
   }
  
-  def subListRefl[T](l: List[T]): Unit = {
- 
+  def subListRefl[T](@induct l: List[T]): Unit = {
+    
   }.ensuring(_ =>
     subList(l, l)
   )
  
   def subListTail[T](l1: List[T], l2: List[T]): Unit = {
     require(!l1.isEmpty && subList(l1, l2))
- 
+    assert(!l2.isEmpty)
+
+    (l1, l2) match {
+      case (Cons(x, xs), Cons(y, ys)) => {
+        if (x == y && subList(xs, ys)){
+          assert(subList(l1.tail, l2))
+        } else { 
+          assert(subList(l1, l2.tail))
+          subListTail(l1, l2.tail)
+        }
+      }
+    }
+
+
   }.ensuring(_ =>
     subList(l1.tail, l2)
   )
  
   def subListTails[T](l1: List[T], l2: List[T]): Unit = {
     require(!l1.isEmpty && !l2.isEmpty && l1.head == l2.head && subList(l1, l2))
+    
+    subListTail(l1, l2)
  
   }.ensuring(_ =>
     subList(l1.tail, l2.tail)
@@ -32,6 +47,31 @@ object SubList {
  
   def subListTrans[T](l1: List[T], l2: List[T], l3: List[T]): Unit = {
     require(subList(l1, l2) && subList(l2, l3))
+    
+    (l1, l2, l3) match {
+        case (Cons(x, xs), Cons(y, ys), Cons(z, zs)) =>
+            (x == y && subList(xs, ys), y == z && subList(ys, zs)) match {
+                case (true, true) => {
+                  subListTrans(xs, ys, zs) 
+                  assert(subList(l1, l3))
+                }
+                case (true, false) => {
+                  subListRefl(l2)
+                  subListTail(l2, l2)
+                  subListTrans(l1, l2, zs)
+                  assert(subList(l1, l3))
+                }
+                case (false, true) => {
+                  subListTail(l1, l2)
+                  subListTrans(l1, ys, zs)
+                }
+                case (false, false) => {
+                  subListTail(l2, l3)
+                  subListTrans(l1, ys, zs)
+                }
+            }
+        case _ => ()
+    }
  
   }.ensuring(_ =>
     subList(l1, l3)
@@ -39,6 +79,12 @@ object SubList {
  
   def subListLength[T](l1: List[T], l2: List[T]): Unit = {
     require(subList(l1, l2))
+    
+    (l1, l2) match {
+        case (Cons(x, xs), Cons(y, ys)) if x == y && subList(xs, ys) => subListLength(xs, ys)
+        case (Cons(x, xs), Cons(y, ys)) => subListLength(l1, ys)
+        case _ => ()
+    }
  
   }.ensuring(_ =>
     l1.length <= l2.length
@@ -46,13 +92,24 @@ object SubList {
  
   def subListEqual[T](l1: List[T], l2: List[T]): Unit = {
     require(subList(l1, l2) && l1.length >= l2.length)
- 
+    
+    (l1, l2) match {
+        case (Cons(x, xs), Cons(y, ys)) if x == y && subList(xs, ys) => subListEqual(xs, ys)
+        case (Cons(x, xs), Cons(y, ys)) => subListEqual(l1, ys)
+        case _ => ()
+    }
+    
   }.ensuring(_ =>
     l1 == l2
   )
  
   def subListAntisym[T](l1: List[T], l2: List[T]): Unit = {
     require(subList(l1, l2) && subList(l2, l1))
+ 
+    (l1, l2) match {
+        case (Cons(x, xs), Cons(y, ys)) => subListLength(l1, l2); subListLength(l2, l1); subListEqual(l1, l2)
+        case _ => ()
+    }
  
   }.ensuring(_ =>
     l1 == l2
