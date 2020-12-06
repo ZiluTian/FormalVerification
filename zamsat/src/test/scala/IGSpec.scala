2b1d2cfa0747af07b4da9d43d2a66e442ce10fcb
ZiluTian <zilu.tian@epfl.ch>
2020-12-06 20:26:30 +0100

    Add the test file for different functions of the IG

---
+++ zamsat/src/test/scala/IGSpec.scala
@@ -0,0 +1,75 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import zamsat.IG
+import zamsat.IG._
+
+import scala.collection.immutable.List
+import scala.collection.mutable.ListBuffer
+
+class IGSpec extends AnyFlatSpec {
+
+  // Build the graph as in https://www.cs.princeton.edu/courses/archive/fall13/cos402/readings/SAT_learning_clauses.pdf on page 8
+  val n1 = DecisionNode(-1, 4)
+
+  val n2 = ImpliedNode(2, 4)
+  val n3 = ImpliedNode(3, 4)
+  val n4 = ImpliedNode(4, 4)
+  val n5 = ImpliedNode(5, 4)
+  val n6 = ImpliedNode(6, 4)
+  val n7 = ImpliedNode(-7, 1)
+  val n8 = ImpliedNode(-8, 2)
+  val n9 = ImpliedNode(-9, 3)
+  val n10 = ImpliedNode(-5, 4)
+
+  val e1 = Edge(n1, n2)
+  val e2 = Edge(n1, n3)
+  val e3 = Edge(n7, n3)
+  val e4 = Edge(n2, n4)
+  val e5 = Edge(n3, n4)
+  val e6 = Edge(n8, n5)
+  val e7 = Edge(n4, n5)
+  val e8 = Edge(n1, n3)
+  val e9 = Edge(n4, n6)
+  val e10 = Edge(n9, n6)
+  val e11 = Edge(n6, n10)
+  //    val e12 = Edge(n5, n10)
+
+  var sampleGraph: IG = new IG(List(), List(n2, n3, n4, n5, n6, n7, n8, n9), List(e1, e2, e3, e4, e5, e6, e7, e8, e9))
+//  val sampleGraph: IG = new IG(List(n1), List(n2, n3, n4, n5, n6, n7, n8, n9, n10), List(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11))
+
+  // If tested individually, please use the commented out sampleGraph. Otherwise graph incomplete
+  "Build graph, add nodes and add edges" should "work" in {
+    sampleGraph = sampleGraph.add(n1)
+    sampleGraph = sampleGraph.add(n10)
+    sampleGraph = sampleGraph.add(e10)
+    sampleGraph = sampleGraph.add(e11)
+  }
+
+  "Conflict node" should "detect node 5" in {
+    assert(sampleGraph.conflictNode(4) == Some(n5))
+  }
+
+  "All paths" should "identify all the paths between two nodes" in {
+
+    val path1: Set[List[Node]] = sampleGraph.allPaths(n1, n10)
+    val path2: Set[List[Node]] = sampleGraph.allPaths(n1, n5)
+
+    assert(path1.size == 2)
+    assert(path2.size == 2)
+  }
+
+  "UIPS" should "return n1 and n4" in {
+    val uips: List[Node] = sampleGraph.UIPS(4)
+    assert(uips == List(n1, n4))
+  }
+
+  "OneUIP" should "return List(n8, n4, n9)" in {
+    val oneUIP: List[Node] = sampleGraph.OneUIP(4)
+    assert(oneUIP == List(n8, n4, n9))
+  }
+
+  "LastUIP" should "return List(n8, n1, n7, n9)" in {
+    val lUIP: List[Node] = sampleGraph.LastUIP(4)
+    assert(lUIP == List(n7, n8, n9, n1))
+  }
+
+}
